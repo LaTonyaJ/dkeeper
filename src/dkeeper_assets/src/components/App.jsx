70a6ad104e8336b9755ed72300b923df210bc2fe
LaTonya Johnson <latonyajohnson@LaTonyas-Air.lan>
2022-09-21 23:42:49 -0500

    CRUD

--- src/dkeeper_assets/src/components/App.jsx
+++ src/dkeeper_assets/src/components/App.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeeper } from "./../../../declarations/dkeeper";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote]
+      dkeeper.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes]
     });
+  }
+
+  useEffect(() =>{
+    showList();
+  }, []);
+
+  async function showList(){
+    const noteArray = await dkeeper.readNotes();
+    setNotes(noteArray);
   }
 
   function deleteNote(id) {
     // console.log(id)
     setNotes((prevNotes) => {
       return prevNotes.filter((note, idx) => {
+        dkeeper.removeNote(id);
         return idx !== id
       })
     });
